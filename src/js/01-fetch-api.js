af01c7ddc2c7fe20d800b6e2cd608272b445d480
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-11-01 11:42:49 +0200

    fox2

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -13,45 +13,59 @@
  * - https://pokeapi.co/
  */
 
-// import '../css/common.css';
-// import pokemonCardTpl from '../templates/pokemon-card.hbs';
-import API from './api-service';
-import getRefs from './get-refs';
+import '../css/common.css';
+import pokemonCardTpl from '../templates/pokemon-card.hbs';
 
-const refs = getRefs();
+fetch('https://pokeapi.co/api/v2/pokemon')
+  .then(response => {
+    return response.json();
+  })
+  .then(pokemon => {
+    console.log(pokemon);
+    const markup = pokemonCardTpl(pokemon);
+    console.log(markup);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
-refs.searchForm.addEventListener('submit', onSearch);
+// import API from './api-service';
+// import getRefs from './get-refs';
 
-function onSearch(e) {
-  e.preventDefault();
+// const refs = getRefs();
 
-  const form = e.currentTarget;
-  const searchQuery = form.elements.query.value;
+// refs.searchForm.addEventListener('submit', onSearch);
 
-  API.fetchPokemon(searchQuery)
-    .then(renderPokemonCard)
-    .catch(onFetchError)
-    .finally(() => form.reset());
-}
+// function onSearch(e) {
+//   e.preventDefault();
 
-function renderPokemonCard(pokemon) {
-  const markup = pokemonCardTpl(pokemon);
-  refs.cardContainer.innerHTML = markup;
-}
+//   const form = e.currentTarget;
+//   const searchQuery = form.elements.query.value;
 
-function onFetchError(error) {
-  alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
-}
+//   API.fetchPokemon(searchQuery)
+//     .then(renderPokemonCard)
+//     .catch(onFetchError)
+//     .finally(() => form.reset());
+// }
 
-// =========================================
+// function renderPokemonCard(pokemon) {
+//   const markup = pokemonCardTpl(pokemon);
+//   refs.cardContainer.innerHTML = markup;
+// }
 
-const url = 'https://newsapi.org/v2/everything?q=cars';
-const options = {
-  headers: {
-    Authorization: '4330ebfabc654a6992c2aa792f3173a3',
-  },
-};
+// function onFetchError(error) {
+//   alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
+// }
 
-fetch(url, options)
-  .then(r => r.json())
-  .then(console.log);
+// // =========================================
+
+// const url = 'https://newsapi.org/v2/everything?q=cars';
+// const options = {
+//   headers: {
+//     Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+//   },
+// };
+
+// fetch(url, options)
+//   .then(r => r.json())
+//   .then(console.log);
